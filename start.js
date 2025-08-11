708f1aa48296e6c3f33e8f0af6c17e0823b5e883
Gorciu <gorciuyt@gmail.com>
2025-08-11 09:50:10 +0200

    another hosting shit v4

--- start.js
+++ start.js
@@ -1,6 +1,12 @@
 // start.js
 const { spawn } = require('child_process');
 
+
+spawn('npm', ['install'], {
+    stdio: 'inherit', 
+    shell: true 
+});
+
 spawn('npm', ['start'], {
     stdio: 'inherit', 
     shell: true 
