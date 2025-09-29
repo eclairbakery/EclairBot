ef513b1f3803bdfa6e40ff5a99ef6063408300ac
Gorciu <gorciuyt@gmail.com>
2025-09-29 17:37:56 +0200

    [feature] channel configuration v2

---
+++ src/util/makeChannelName.ts
@@ -0,0 +1,15 @@
+import { cfg } from "@/bot/cfg.js";
+
+function makeEmojiForChannelName(emoji: string) {
+    return `${cfg.channelsConfiguration.characters.beforeEmoji}${emoji.replace(' ', cfg.channelsConfiguration.spaceReplacement ?? '-')}${cfg.channelsConfiguration.characters.afterEmoji}`;
+};
+
+export function makeChannelName(data: { name: string; emoji: string; }) {
+    if (data.emoji.length > 1) {
+        console.log(`Suspicious channel emoji at makeChannelName (data: ${JSON.stringify(data)})`);
+    }
+    if (data.name.length < 3) {
+        console.log(`Suspicious channel name at makeChannelName (data: ${JSON.stringify(data)})`);
+    }
+    return `${cfg.channelsConfiguration.emojiPlacement == 'before-name' ? makeEmojiForChannelName(data.emoji) : '' }${data.name.replace(' ', cfg.channelsConfiguration.spaceReplacement ?? '-')}${cfg.channelsConfiguration.emojiPlacement == 'after-name' ? makeEmojiForChannelName(data.emoji) : '' }`;
+}
