3cf3c5a28b855ad62a7d21009f03910624fed131
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-03-06 23:36:34 +0100

    [feature] cache utils

---
+++ src/bot/apis/cache/cache.ts
@@ -0,0 +1,52 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+function getCacheDir(): string {
+    const cacheHome = process.env.XDG_CACHE_HOME;
+    if (cacheHome) {
+        return path.join(cacheHome, 'eclairbot');
+    }
+
+    const home = process.env.HOME;
+    if (home) {
+        return path.join(home, '.cache', 'eclairbot');
+    }
+
+    return path.join('/', 'tmp', 'eb-cache');
+}
+
+export async function initCache() {
+    await fs.mkdir(getCacheDir(), { recursive: true });
+}
+
+function getBoxFilepath(box: string): string {
+    return path.join(getCacheDir(), box + '.json');
+}
+
+async function readBox(boxpath: string): Promise<Record<string, any>> {
+    try {
+        const content = await fs.readFile(boxpath, 'utf8');
+        return JSON.parse(content);
+    } catch (err: any) {
+        if (err.code === 'ENOENT') {
+            return {};
+        }
+        throw err;
+    }
+}
+
+export async function store<T>(box: string, key: string, value: T) {
+    const boxpath = getBoxFilepath(box);
+
+    const json = await readBox(boxpath);
+    json[key] = value;
+
+    await fs.writeFile(boxpath, JSON.stringify(json));
+}
+
+export async function load<T>(box: string, key: string): Promise<T | undefined> {
+    const boxpath = getBoxFilepath(box);
+
+    const json = await readBox(boxpath);
+    return json[key] as T | undefined;
+}
