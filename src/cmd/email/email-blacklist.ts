51df5edba4622c9bcb22feeee70524477cbb0118
Gorciu <gorciuyt@gmail.com>
2026-03-10 17:49:22 +0100

    [improvment] do not require the lock/unlock argument in email blacklist

--- src/cmd/email/email-blacklist.ts
+++ src/cmd/email/email-blacklist.ts
@@ -6,7 +6,7 @@ export const emailBlacklistCmd: Command = {
     aliases: ['email-add-blacklisted-email'],
     description: {
         main: "Blacklistujesz e-mail i nie możesz już od niego odbierać maili (tzn. są kwalifikowane jako spam) ani wysyłać maili do tej osoby.",
-        short: "Daje e-mail na blacklistę."
+        short: "Dodaje lub usuwa e-mail z blacklisty."
     },
 
     permissions: CommandPermissions.devOnly(),
@@ -14,39 +14,43 @@ export const emailBlacklistCmd: Command = {
 
     expectedArgs: [
         {
-            name: 'type',
-            description: "Dajesz unlock by odblokować lub lock by zablokować.",
-            optional: false,
-            type: 'string'
-        },
-        {
             name: 'email',
-            description: "Ten email co chcesz zblacklistować",
+            description: "Email który chcesz zblacklistować lub odblokować.",
             optional: false,
             type: 'string'
         }
     ],
 
     async execute(api) {
-        const mail = api.getTypedArg('email', 'string').value!;
-        const btype = api.getTypedArg('type', 'string').value!;
+        const mail = api.getTypedArg('email', 'string').value!.toLowerCase();
 
-        let unlock = btype.trim().toLowerCase() === 'unlock';
+        const exists = await db.selectOne(
+            'SELECT 1 FROM email_blacklist WHERE email = ? LIMIT 1',
+            [mail]
+        );
 
-        if (unlock) {
+        if (exists) {
             await db.runSql(
-                'DELETE from email_blacklist WHERE email = ?',
-                [ mail.toLowerCase() ]
+                'DELETE FROM email_blacklist WHERE email = ?',
+                [mail]
             );
 
-            return api.log.replySuccess(api, 'Udało się!', 'Teraz ten e-mail **nie** jest zblacklistowany.')
+            return api.log.replySuccess(
+                api,
+                'Usunięto!',
+                'Ten e-mail **nie jest już** na blacklistcie.'
+            );
         }
 
         await db.runSql(
-            'INSERT OR IGNORE INTO email_blacklist VALUES(NULL, ?)',
-            [ mail.toLowerCase() ]
+            'INSERT INTO email_blacklist VALUES (NULL, ?)',
+            [mail]
         );
 
-        return api.log.replySuccess(api, 'Yay!', 'Teraz ten e-mail jest zblacklistowany.')
+        return api.log.replySuccess(
+            api,
+            'Dodano!',
+            'Ten e-mail **jest teraz** na blacklistcie.'
+        );
     },
 };
