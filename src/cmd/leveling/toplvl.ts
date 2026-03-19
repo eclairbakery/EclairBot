f81413ea93f5c1269515741f2ef6843a2124b823
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-03-19 21:04:59 +0100

    container -> embed
    reverting changes since embed looked so much nicer

--- src/cmd/leveling/toplvl.ts
+++ src/cmd/leveling/toplvl.ts
@@ -1,12 +1,12 @@
-import * as dsc from 'discord.js';
+import * as dsc from "discord.js";
 
-import { cfg } from '@/bot/cfg.ts';
-import { lvlRoles } from '@/bot/level.ts';
+import { cfg } from "@/bot/cfg.ts";
+import { lvlRoles } from "@/bot/level.ts";
 
-import { Command } from '@/bot/command.ts';
-import { CommandFlags } from '@/bot/apis/commands/misc.ts';
-import { output } from '@/bot/logging.ts';
-//import { ReplyEmbed } from "@/bot/apis/translations/reply-embed.ts";
+import { Command } from "@/bot/command.ts";
+import { CommandFlags } from "@/bot/apis/commands/misc.ts";
+import { output } from "@/bot/logging.ts";
+import { ReplyEmbed } from "@/bot/apis/translations/reply-embed.ts";
 
 function calculateLevel(xp: number, levelDivider: number): number {
     return Math.floor(
@@ -15,11 +15,11 @@ function calculateLevel(xp: number, levelDivider: number): number {
 }
 
 export const toplvlCmd: Command = {
-    name: 'toplvl',
-    aliases: ['topka', 'toplevel'],
+    name: "toplvl",
+    aliases: ["topka", "toplevel"],
     description: {
-        main: 'Czas popatrzeć na najlepszych użytkowników serwera...',
-        short: 'Czas popatrzeć na najlepszych użytkowników serwera...',
+        main: "Czas popatrzeć na najlepszych użytkowników serwera...",
+        short: "Czas popatrzeć na najlepszych użytkowników serwera...",
     },
     flags: CommandFlags.None,
 
@@ -30,57 +30,57 @@ export const toplvlCmd: Command = {
     expectedArgs: [],
 
     async execute(api) {
-        const rows = await api.executor.leveling.getEveryoneXPWithLimit(50);
+        try {
+            const rows = await api.executor.leveling.getEveryoneXPWithLimit(50);
 
-        if (rows.length == 0) {
-            await api.reply('Nie ma żadnego w bazie poziomu :sob:');
-        }
+            if (rows.length == 0) {
+                await api.reply("Nie ma żadnego w bazie poziomu :sob:");
+            }
 
-        const fields: dsc.APIEmbedField[] = [];
-        let i = 0;
+            const fields: dsc.APIEmbedField[] = [];
+            let i = 0;
 
-        for (const row of rows) {
-            if (++i > 12) break;
+            for (const row of rows) {
+                if (++i > 12) break;
 
-            try {
-                const member = await api.guild?.members.fetch(row.user_id);
-                if (!member) {
+                try {
+                    const member = await api.guild?.members.fetch(row.user_id);
+                    if (!member) {
+                        i--;
+                        continue;
+                    }
+
+                    const userLvlRole = lvlRoles.filter((id) => member.roles.cache.has(id)).at(-1);
+                    fields.push({
+                        name: `${i} » ${member.user.username}`,
+                        value: `${userLvlRole ? `<@&${userLvlRole}>` : "Nowicjusz..."}\n**Lvl**: ${calculateLevel(row.xp, cfg.features.leveling.levelDivider)}\n**XP**: ${row.xp}${i % 2 === 1 ? "‎" : ""}`,
+                        inline: true,
+                    });
+                } catch (e) {
+                    output.warn(e);
                     i--;
                     continue;
                 }
-
-                const userLvlRole = lvlRoles.filter((id) => member.roles.cache.has(id)).at(-1);
-                fields.push({
-                    name: `${i} » ${member.user.username}`,
-                    value: `${userLvlRole ? `<@&${userLvlRole}>` : 'Nowicjusz...'}\n**Lvl**: ${calculateLevel(row.xp, cfg.features.leveling.levelDivider)}\n**XP**: ${row.xp}${i % 2 === 1 ? '‎' : ''}`,
-                    inline: true,
-                });
-            } catch (e) {
-                output.warn(e);
-                i--;
-                continue;
             }
+
+            const serverXP = await api.executor.leveling.getTotalServerXP();
+
+            await api.reply({
+                embeds: [
+                    new ReplyEmbed()
+                        .setColor("#1ebfd5")
+                        .setImage("https://cdn.discordapp.com/attachments/1404396223934369844/1404397238578577491/toplvl_image.png?ex=689b0a5a&is=6899b8da&hm=eac2a0db46bfad2dd34fa1ef8dbf9b918e46913229f7b1a9c470d952982787e8&"),
+                    new ReplyEmbed()
+                        .setFields(fields)
+                        .setColor("#1ebfd5")
+                        .setFooter({
+                            text: `Poziom serwera: ${calculateLevel(serverXP, cfg.features.leveling.levelDivider)} LVL (XP: ${serverXP})`,
+                        }),
+                ],
+            });
+        } catch (err) {
+            output.err(err);
+            await api.reply("❌ Wystąpił błąd podczas pobierania topu poziomów.");
         }
-
-        const serverXP = await api.executor.leveling.getTotalServerXP();
-
-        await api.reply({
-            components: [
-                new dsc.MediaGalleryBuilder()
-                    .addItems(
-                        (mgi) => mgi.setDescription('toplvl image').setURL('https://cdn.discordapp.com/attachments/1404396223934369844/1404397238578577491/toplvl_image.png?ex=689b0a5a&is=6899b8da&hm=eac2a0db46bfad2dd34fa1ef8dbf9b918e46913229f7b1a9c470d952982787e8&'),
-                    ),
-                new dsc.SeparatorBuilder().setSpacing(dsc.SeparatorSpacingSize.Large),
-                new dsc.TextDisplayBuilder()
-                    .setContent('- ' + fields.map((f) => `**${f.name}**: ${f.value}`).join('\n- ')),
-                new dsc.SeparatorBuilder().setSpacing(dsc.SeparatorSpacingSize.Large),
-                new dsc.TextDisplayBuilder()
-                    .setContent(`Poziom serwera: ${calculateLevel(serverXP, cfg.features.leveling.levelDivider)} (${serverXP} XP)`),
-            ],
-            flags: dsc.MessageFlags.IsComponentsV2,
-            allowedMentions: {
-                parse: [],
-            },
-        });
     },
 };
