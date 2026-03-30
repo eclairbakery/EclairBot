4012e0b303cf84a0d270222902890b603fc62255
Gorciu <gorciuyt@gmail.com>
2026-03-30 18:45:19 +0200

    [improvment] bloatware removal: simplify quote bot

--- src/features/actions/others/basicMsgCreateActions.ts
+++ src/features/actions/others/basicMsgCreateActions.ts
@@ -24,46 +24,16 @@ export const basicMsgCreateActions: Action<MessageEventCtx> = {
             // quote bot
             await (async function () {
                 if (msg.author.bot) return;
+
                 const regex = /https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(\d+)\/(\d+)\/(\d+)/;
                 const match = msg.content.match(regex);
                 if (!match) return;
                 const [, , channelId, messageId] = match;
+
                 const channel = await msg.client.channels.fetch(channelId);
                 if (!channel?.isTextBased()) return;
                 if (channel.isDMBased()) return;
-
-                const perms = (channel as GuildTextBasedChannel).permissionsFor(msg.author);
-                if (
-                    !perms?.has(PermissionsBitField.Flags.ViewChannel) ||
-                    !perms?.has(PermissionsBitField.Flags.ReadMessageHistory)
-                ) {
-                    return log.replyInfo(msg, 'Co ty odsigmiasz bratku?', 'Nie cytuj wiadomości których nawet nie możesz zobaczyć...');
-                }
-
-                const excludedChannels = ['1235641912241819669'];
-                const strictlyExcludedChannels = ['1290327060970995812'];
-                const excludedCategories = ['1235552673978388560', '1419322858659905566', '1407377025437798442'];
-
-                if (strictlyExcludedChannels.includes(channel.id)) {
-                    return log.replyWarn(
-                        msg,
-                        'Ten kanał jest zablokowany!',
-                        'Jest on na liście kanałów ściśle zablokowanych, więc nie możesz z tamtąd niczego zleakować everyone... so sad, so sad...',
-                    );
-                } else if (excludedChannels.includes(channel.id) && !msg.content.includes('--no-block-excluded-channels')) {
-                    return log.replyInfo(
-                        msg,
-                        'Ten kanał jest zablokowany!',
-                        'Z racji iż miły jestem czy coś i masz permisje do kanału, to możesz ponowić quote z `--no-block-excluded-channels`.',
-                    );
-                } else if (channel.parentId && excludedCategories.includes(channel.parentId) && !msg.content.includes('--no-block-excluded-channels')) {
-                    return log.replyInfo(
-                        msg,
-                        'Ten kanał jest w zablokowanej kategorii!',
-                        'Z racji iż miły jestem czy coś i masz permisje do kanału, to możesz ponowić quote z `--no-block-excluded-channels`.',
-                    );
-                }
-
+ 
                 const quotedMsg = await channel.messages.fetch(messageId);
 
                 const embed = new ReplyEmbed()
