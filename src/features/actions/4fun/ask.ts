dbbb1035cd1ef86091e5e0748adaf847c58d8031
Gorciu <gorciuyt@gmail.com>
2026-04-18 19:06:44 +0200

    [patch] fix message refrence handling for the next time

--- src/features/actions/4fun/ask.ts
+++ src/features/actions/4fun/ask.ts
@@ -14,7 +14,7 @@ export const askAction: Action<MessageEventCtx> = {
         async (ctx) =>
             ctx.channelId == cfg.channels.general.ei ||
             ctx.content.trim().startsWith(`<@${client.user?.id}>`) ||
-            ctx.reference?.messageId 
+            typeof ctx.reference?.messageId == 'string'
                 ? (await ctx.fetchReference()).author.id == client.user!.id
                 : false,
         (ctx) =>
