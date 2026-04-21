f6dddedb2cdf8f6c00385e01e9948442a65099ed
Gorciu <gorciuyt@gmail.com>
2026-04-21 16:21:27 +0200

    [improvment] use allowedMentions, not usernames

--- src/features/actions/others/welcomer.ts
+++ src/features/actions/others/welcomer.ts
@@ -36,7 +36,11 @@ export const welcomeNewUserAction: Action<UserEventCtx> = {
                 await welcomeChannel.send('Spierdalaj ty zjebie podludzki start it nikt cię tu nie chce');
                 return;
             } else {
-                await welcomeChannel.send('<:emoji1:1410551894023082027>' + cfg.features.welcomer.welcomeMsgs[Math.floor(Math.random() * cfg.features.welcomer.welcomeMsgs.length)].replace('<mention>', cfg.features.welcomer.mentionNewPeopleInLobby ? `<@${member.user.id}>` : member.user.username));
+                await welcomeChannel.send({
+                    content: '<:emoji1:1410551894023082027>' + 
+                        cfg.features.welcomer.welcomeMsgs[Math.floor(Math.random() * cfg.features.welcomer.welcomeMsgs.length)].replace('<mention>', `<@${member.user.id}>`),
+                    allowedMentions: (cfg.features.welcomer.mentionNewPeopleInLobby ? {} : { parse: [] })
+                });
                 await generalChannel.send(`witaj <@${member.user.id}>, będzie nam miło jak się przywitasz czy coś <:emoji_a_radosci_nie_bylo_konca:1376664467416420362>`);
             }
         },
