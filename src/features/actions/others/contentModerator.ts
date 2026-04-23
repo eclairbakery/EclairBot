02ab7f08321abd0c925815799c44ae7c70021b3c
Gorciu <gorciuyt@gmail.com>
2026-04-23 16:29:03 +0200

    [improvment] new community polls handling

--- src/features/actions/others/contentModerator.ts
+++ src/features/actions/others/contentModerator.ts
@@ -1,4 +1,4 @@
-import { Action, MessageEventCtx, PredefinedActionEventTypes } from '@/features/actions/index.ts';
+import { type Action, type MessageEventCtx, PredefinedActionEventTypes } from '@/features/actions/index.ts';
 import { cfg } from '@/bot/cfg.ts';
 import sleep from '@/util/sleep.ts';
 import { replyWarn } from '@/util/log.ts';
@@ -20,6 +20,31 @@ export const filesContentModerator: Action<MessageEventCtx> = {
             await reply.delete();
             await msg.delete();
             await msg.channel.delete();
+        }
+    ]
+};
+
+export const communityPollsContentModerator: Action<MessageEventCtx> = {
+    activationEventType: PredefinedActionEventTypes.OnMessageCreate,
+
+    constraints: [
+        (ctx) => ctx.author.id != ctx.client.user.id,
+        (ctx) => !ctx.channel.isThread(),
+        (ctx) => ctx.channel.id == cfg.channels.other.communityPolls
+    ],
+    callbacks: [
+        async (msg) => {
+            if (!msg.poll) {
+                return replyWarn(msg, 'To nie do tego kanał', 'W skrócie no to na kanale, który się nazywa "ankiety społeczności" wysyła się te Discordowe ankiety.');
+            }
+
+            await msg.reply('<@1320034068322324660>');
+            
+            const threadNames = ["Kanał commentary na YT", "Odpowiedzi", "Komentarze", "Merytoryczne dyskusje na temat ankiety"];
+            const thread = await msg.startThread({
+                name: threadNames[Math.floor(Math.random() * threadNames.length)],
+            });
+            await thread.send('tu możecie odpowiedzi merytoryczne wysyłać');
         }
     ]
 };
